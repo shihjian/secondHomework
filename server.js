ead2fa58cf19f191cb90426a6c5040e3ee7b3ec6
YJ <werty713025@gmail.com>
2022-04-19 21:38:27 +0800

    test something

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB)
   .then(() => {
-    console.log("資料庫連線成功");
+    console.log("資料庫連線成功哦");
   })
   .catch((error) => {
     console.log(error);
